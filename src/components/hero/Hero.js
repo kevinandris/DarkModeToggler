8baa1bed4b29dd610fdfde9e0cc47a20ab632c08
keipy27 <kevinandris27@gmail.com>
2023-08-05 12:19:18 +1200

    hero.js file code implemented

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './Hero.css'
+import heroImg from '../../assets/phone.svg' 
 
 const Hero = () => {
+
   return (
-    <div>Hero</div>
+    <section className='hero'>
+      <div className="container">
+        <div className="hero-text">
+          <h1>Visit Vila Shop Landing Page</h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti soluta laboriosam illum commodi fugit, quia dolorem tenetur animi modi culpa nisi tempora nihil maxime nostrum necessitatibus pariatur nam corrupti neque.</p>
+        </div>
+
+        <div>
+          <button>Learn More</button>
+          <button>Sign Up</button>
+        </div>
+
+        <div className="hero-image">
+          <img src={heroImg} alt="phone" width={170}/>
+        </div>
+      </div>
+    </section>
   )
 }
 
